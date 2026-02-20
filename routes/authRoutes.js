718ee4874e2ad40eb4c0ab588d11ab558fbe5154
JaquesBavurhe <johnsonbavurhe60@gmail.com>
2026-02-20 12:33:30 +0300

    improved auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -28,24 +28,30 @@ router.post("/", async (req, res) => {
 
 // Login
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  let _user = await User.findOne({ email });
-  if (_user) {
-    let user = {
-      id: _user._id,
-      name: _user.name,
-      role: _user.role,
-    };
-    const isMatch = await bcrypt.compare(password, _user.password);
-    if (isMatch) {
-      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: "1h"});
-      res.status(200).json({ token, user });
+    let _user = await User.findOne({ email });
+    if (_user) {
+      let user = {
+        sub: _user._id,
+        name: _user.name,
+        role: _user.role,
+      };
+      const isMatch = await bcrypt.compare(password, _user.password);
+      if (isMatch) {
+        const token = jwt.sign(user, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        res.status(200).json({ token, user });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
     } else {
-      res.status(401).json({ message: "Invalid credentials" });
+      res.status(401).json({ message: "User not found" });
     }
-  } else {
-    res.status(401).json({ message: "User not found" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
